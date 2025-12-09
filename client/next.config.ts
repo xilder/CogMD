d36dcf4d199f3807a8a7236ef4f1e4242a22cb34
Abel Fagbemi <olabowaleayobami@gmail.com>
2025-12-09 20:56:32 +0100

    minor v11

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  distDir: 'build',
+  output: 'standalone',
+  // distDir: 'build',
   images: { unoptimized: false },
 };
 
